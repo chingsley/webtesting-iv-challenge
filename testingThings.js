bd3fceb91a4655d394bffd53c1f7bd673bee8d44
chingsley <eneja.kc@gmail.com>
2020-06-12 21:17:01 +0100

    testing things

--- testingThings.js
+++ testingThings.js
@@ -4,3 +4,7 @@ const { isVAlidEmail } = require('./src/api/middlewares/helpers');
 console.log(moment().format());
 
 console.log(isVAlidEmail('eneja_kc@gmail.net'));
+// testing things 1
+// testing things 2
+// testing things 3
+// testing things 4
